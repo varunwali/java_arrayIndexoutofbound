3105e076fa966ef0f69aa64d0540de0b68864f39
varunwali <92247037+varunwali@users.noreply.github.com>
2023-04-04 22:00:33 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> li = new ArrayList<>();
+        li.add(10);
+        li.add(7);
+        li.add(9);
+        System.out.println(li);
+
+    }
+}
